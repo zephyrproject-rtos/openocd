2e8bd011a3b351ef4a19350f4ed81cc90e9edb19
Frank Dols <frank.dols@synopsys.com>
2013-03-01 15:36:08 +0100

    ARC: introduce the arc700 to src/target
    Signed-off-by: Frank Dols <frank.dols@synopsys.com>

---
+++ src/target/arc700.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2012 Synopsys, Inc. (www.synopsys.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ *   Maintainer: frank.dols@synopsys.com
+ */
+
+#ifndef ARC_700_H
+#define ARC_700_H
+
+#include "target.h"
+#include "arc32.h"
+#include "arc_jtag.h"
+
+
+#define ARC700_COMMON_MAGIC 0x1A471AC5  /* just a unique number */
+#define ARC700_NUM_REGS		32
+
+#define SYSTEM_CONTROL_BASE 0x400FE000  // needs check
+#define CPUID				0xE000ED00  // needs check
+
+struct arc700_common {
+	int common_magic;
+	bool is_4wire; // was:  is_pic32mx; in mips_4k context
+	struct arc32_common arc32;
+};
+
+
+#endif /* ARC700_H */
