c66de51ca443a49c7c5f9ea4561f4b021a78e6b7
Frank Dols <frank.dols@synopsys.com>
2013-04-05 16:56:45 +0200

    ARC: migrate to the one "arc.h" include
    Signed-off-by: Frank Dols <frank.dols@synopsys.com>

---
+++ src/target/arc.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2012 Synopsys, Inc. (www.synopsys.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ *   Maintainer: frank.dols@synopsys.com
+ */
+
+#ifndef ARC_H
+#define ARC_H
+
+#include <helper/time_support.h>
+#include <jtag/jtag.h>
+
+#include "algorithm.h"
+#include "breakpoints.h"
+#include "jtag/interface.h"
+#include "register.h"
+#include "target.h"
+#include "target_request.h"
+#include "target_type.h"
+
+#include "arc32.h"
+#include "arc700.h"
+#include "arc_jtag.h"
+#include "arc_mntr.h"
+#include "arc_regs.h"
+
+
+
+#endif /* ARC_H */
