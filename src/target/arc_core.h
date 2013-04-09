29270467f6b6ec073d1786f913b25e35e6967b87
Frank Dols <frank.dols@synopsys.com>
2013-04-09 13:58:19 +0200

    ARC: core functions stack module interface
    Signed-off-by: Frank Dols <frank.dols@synopsys.com>

---
+++ src/target/arc_core.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2012 Synopsys, Inc. (www.synopsys.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ *   Maintainer: frank.dols@synopsys.com
+ */
+
+#ifndef ARC_CORE_H
+#define ARC_CORE_H
+
+
+/* ----- Exported functions ------------------------------------------------ */
+
+int arc_core_soft_reset_halt_imp(struct target *target);
+
+int arc_core_soft_reset_halt(struct target *target);
+
+#endif /* ARC_CORE_H */
