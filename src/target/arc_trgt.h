cfae943fb3f25df462587e61b8cc0666315445af
Frank Dols <frank.dols@synopsys.com>
2013-04-09 13:57:28 +0200

    ARC: target functions stack module interface
    Signed-off-by: Frank Dols <frank.dols@synopsys.com>

---
+++ src/target/arc_trgt.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2012 Synopsys, Inc. (www.synopsys.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ *   Maintainer: frank.dols@synopsys.com
+ */
+
+#ifndef ARC_TRGT_H
+#define ARC_TRGT_H
+
+
+/* ----- Exported functions ------------------------------------------------ */
+
+int arc_trgt_request_data(struct target *target, uint32_t size,
+	uint8_t *buffer);
+
+#endif /* ARC_TRGT_H */
