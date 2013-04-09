00aac81676bbb83af22311555eb57fb98b7bdcb4
Frank Dols <frank.dols@synopsys.com>
2013-04-09 14:59:17 +0200

    ARC: add monitor cmds to read/write core & aux regs
    Signed-off-by: Frank Dols <frank.dols@synopsys.com>

--- src/target/arc_mntr.c
+++ src/target/arc_mntr.c
@@ -47,9 +47,149 @@ COMMAND_HANDLER(handle_set_pc_command)
 			COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], value);
 			LOG_DEBUG("CMD_ARGC:%d  CMD_ARGV: 0x%x\n", CMD_ARGC, value);
 			arc_jtag_write_aux_reg(&arc32->jtag_info, AUX_PC_REG, &value);
-			LOG_USER("Core PC @: 0x%x",value);
+			LOG_USER("Core PC @: 0x%x", value);
 		} else
 			LOG_USER(" > missing address to set.");
+	} else
+		LOG_USER(" > head list is not NULL !");
+
+	return retval;
+}
+
+COMMAND_HANDLER(handle_read_core_reg_command)
+{
+	int retval = ERROR_OK;
+	uint32_t reg_nbr, value;
+
+	LOG_DEBUG(">> Entering <<");
+
+	struct target *target = get_current_target(CMD_CTX);
+	struct arc32_common *arc32 = target_to_arc32(target);
+
+	struct target_list *head;
+	head = target->head;
+
+	if (target->state != TARGET_HALTED) {
+		command_print(CMD_CTX, " NOTE: target must be HALTED for \"%s\" command",
+			CMD_NAME);
+		return ERROR_OK;
+	}
+
+	if (head == (struct target_list *)NULL) {
+
+		if (CMD_ARGC >= 1) {
+			COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], reg_nbr);
+			LOG_DEBUG("CMD_ARGC:%d  CMD_ARGV: 0x%x\n", CMD_ARGC, reg_nbr);
+			arc_jtag_read_core_reg(&arc32->jtag_info, reg_nbr, &value);
+			LOG_USER("Core reg: 0x%x contains: 0x%x", reg_nbr, value);
+		} else
+			LOG_USER(" > missing reg nbr to read.");
+	} else
+		LOG_USER(" > head list is not NULL !");
+
+	return retval;
+}
+
+COMMAND_HANDLER(handle_write_core_reg_command)
+{
+	int retval = ERROR_OK;
+	uint32_t reg_nbr, value;
+
+	LOG_DEBUG(">> Entering <<");
+
+	struct target *target = get_current_target(CMD_CTX);
+	struct arc32_common *arc32 = target_to_arc32(target);
+
+	struct target_list *head;
+	head = target->head;
+
+	if (target->state != TARGET_HALTED) {
+		command_print(CMD_CTX, " NOTE: target must be HALTED for \"%s\" command",
+			CMD_NAME);
+		return ERROR_OK;
+	}
+
+	if (head == (struct target_list *)NULL) {
+
+		if (CMD_ARGC >= 2) {
+			COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], reg_nbr);
+			LOG_DEBUG("CMD_ARGC:%d  CMD_ARGV: 0x%x\n", CMD_ARGC, reg_nbr);
+			COMMAND_PARSE_NUMBER(u32, CMD_ARGV[1], value);
+			LOG_DEBUG("CMD_ARGC:%d  CMD_ARGV: 0x%x\n", CMD_ARGC, value);
+			arc_jtag_write_core_reg(&arc32->jtag_info, reg_nbr, &value);
+			LOG_DEBUG("Core reg: 0x%x contains: 0x%x", reg_nbr, value);
+		} else
+			LOG_USER(" > missing reg nbr or value to write.");
+	} else
+		LOG_USER(" > head list is not NULL !");
+
+	return retval;
+}
+
+COMMAND_HANDLER(handle_read_aux_reg_command)
+{
+	int retval = ERROR_OK;
+	uint32_t reg_nbr, value;
+
+	LOG_DEBUG(">> Entering <<");
+
+	struct target *target = get_current_target(CMD_CTX);
+	struct arc32_common *arc32 = target_to_arc32(target);
+
+	struct target_list *head;
+	head = target->head;
+
+	if (target->state != TARGET_HALTED) {
+		command_print(CMD_CTX, " NOTE: target must be HALTED for \"%s\" command",
+			CMD_NAME);
+		return ERROR_OK;
+	}
+
+	if (head == (struct target_list *)NULL) {
+
+		if (CMD_ARGC >= 1) {
+			COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], reg_nbr);
+			LOG_DEBUG("CMD_ARGC:%d  CMD_ARGV: 0x%x\n", CMD_ARGC, reg_nbr);
+			arc_jtag_read_aux_reg(&arc32->jtag_info, reg_nbr, &value);
+			LOG_USER("AUX reg: 0x%x contains: 0x%x", reg_nbr, value);
+		} else
+			LOG_USER(" > missing reg nbr to read.");
+	} else
+		LOG_USER(" > head list is not NULL !");
+
+	return retval;
+}
+
+COMMAND_HANDLER(handle_write_aux_reg_command)
+{
+	int retval = ERROR_OK;
+	uint32_t reg_nbr, value;
+
+	LOG_DEBUG(">> Entering <<");
+
+	struct target *target = get_current_target(CMD_CTX);
+	struct arc32_common *arc32 = target_to_arc32(target);
+
+	struct target_list *head;
+	head = target->head;
+
+	if (target->state != TARGET_HALTED) {
+		command_print(CMD_CTX, " NOTE: target must be HALTED for \"%s\" command",
+			CMD_NAME);
+		return ERROR_OK;
+	}
+
+	if (head == (struct target_list *)NULL) {
+
+		if (CMD_ARGC >= 2) {
+			COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], reg_nbr);
+			LOG_DEBUG("CMD_ARGC:%d  CMD_ARGV: 0x%x\n", CMD_ARGC, reg_nbr);
+			COMMAND_PARSE_NUMBER(u32, CMD_ARGV[1], value);
+			LOG_DEBUG("CMD_ARGC:%d  CMD_ARGV: 0x%x\n", CMD_ARGC, value);
+			arc_jtag_write_aux_reg(&arc32->jtag_info, reg_nbr, &value);
+			LOG_DEBUG("AUX reg: 0x%x contains: 0x%x", reg_nbr, value);
+		} else
+			LOG_USER(" > missing reg nbr or value to write.");
 	} else
 		LOG_USER(" > head list is not NULL !");
 
@@ -147,8 +287,36 @@ static const struct command_registration arc_core_command_handlers[] = {
 		.name = "set-pc",
 		.handler = handle_set_pc_command,
 		.mode = COMMAND_EXEC,
-		.usage = "<value>",
+		.usage = "has one argument: <value>",
 		.help = "modify the core program counter (PC) register",
+	},
+	{
+		.name = "read-core-reg",
+		.handler = handle_read_core_reg_command,
+		.mode = COMMAND_EXEC,
+		.usage = "has one argument: <reg-nbr>",
+		.help = "list the content of a particular core registers",
+	},
+	{
+		.name = "write-core-reg",
+		.handler = handle_write_core_reg_command,
+		.mode = COMMAND_EXEC,
+		.usage = "has two argument: <reg-nbr> <value to write>",
+		.help = "write value to a particular core registers",
+	},
+	{
+		.name = "read-aux-reg",
+		.handler = handle_read_aux_reg_command,
+		.mode = COMMAND_EXEC,
+		.usage = "has one argument: <reg-nbr>",
+		.help = "list the content of a particular aux registers",
+	},
+	{
+		.name = "write-aux-reg",
+		.handler = handle_write_aux_reg_command,
+		.mode = COMMAND_EXEC,
+		.usage = "has two argument: <reg-nbr> <value to write>",
+		.help = "write value to a particular aux registers",
 	},
 	{
 		.name = "print-core-registers",
