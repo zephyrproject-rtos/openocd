2f5dfff2ba1cf0b816d997c37f4eb412d8467b0e
Anton Kolesov <anton.kolesov@synopsys.com>
2013-11-05 19:38:40 +0400

    ARC: Fix support for big endian memory model
    OpenOCD wasn't working properly with big-endian cores: operations on bytes
    and half-words were done at wrong addresses, word-operations were correct.
    In case of reading problem was happening because half-word or byte was
    extracted from word-aligned array when it was in host-endianness, then
    half-words were converted to target order. However to work instead of words
    should be converted to target endianness first, then desired bytes should be
    extracted. What was happening was that order of bytes in half-words was
    correct, but half-word was itself wrong. Bytes also were took from invalid
    address.
    
    Another problem with existing code was that it implemented unaligned memory
    access. This is not required, because generic code in OpenOCD can split
    unaligned memory access to a series of aligned access. Code in arc_mem.c
    worked only for little-endian host and target (maybe for big-endian host and
    target as well), but not for little-endian host and big-endian target.
    Considering that it is not needed and makes code only more complex I've
    removed support for unaligned memory accesses in both writes and reads.
    OpenOCD generic code splits memory access in a right way for both
    little- and big-endian.
    
    Signed-off-by: Anton Kolesov <anton.kolesov@synopsys.com>

--- src/target/arc_mem.c
+++ src/target/arc_mem.c
@@ -33,34 +33,89 @@ static int arc_mem_read_block(struct arc_jtag *jtag_info, uint32_t addr,
 	return retval;
 }
 
-static int arc_mem_write_block(struct arc_jtag *jtag_info, uint32_t addr,
-			       int size, int count, void *buf)
+/* Write word at word-aligned address */
+static int arc_mem_write_block32(struct target *target, uint32_t addr, int count,
+	void *buf)
 {
+	struct arc32_common *arc32 = target_to_arc32(target);
 	int retval = ERROR_OK;
 	int i;
 
-	assert(size <= 4);
+	/* Check arguments */
+	if (addr & 0x3u)
+		return ERROR_TARGET_UNALIGNED_ACCESS;
 
-	if (size == 4) {
-		for(i = 0; i < count; i++) {
-			retval = arc_jtag_write_memory(jtag_info, (addr + (i * size)),
-				(buf + (i * size)));
-		}
-	} else {
-		/*
-		 * we have to handle the sw-breakpoint here.
-		 * size 2  = half word
+	for(i = 0; i < count; i++) {
+		retval = arc_jtag_write_memory(&arc32->jtag_info,
+			addr + i * sizeof(uint32_t) , buf + i * sizeof(uint32_t));
+		if (retval != ERROR_OK)
+			return retval;
+	}
+
+	return retval;
+}
+
+/* Write half-word at half-word-aligned address */
+static int arc_mem_write_block16(struct target *target, uint32_t addr, int count,
+	void *buf)
+{
+	struct arc32_common *arc32 = target_to_arc32(target);
+	int retval = ERROR_OK;
+	int i;
+
+	/* Check arguments */
+	if (addr & 1u)
+		return ERROR_TARGET_UNALIGNED_ACCESS;
+
+	uint32_t buffer_he;
+	uint8_t buffer_te[sizeof(uint32_t)];
+	uint8_t halfword_te[sizeof(uint16_t)];
+	for(i = 0; i < count; i++) {
+		/* We can read only word at word-aligned address. Also *jtag_read_memory
+		 * functions return data in host endianness, so host endianness !=
+		 * target endianness we have to convert data back to target endianness,
+		 * or bytes will be at the wrong places.So:
+		 *   1) read word
+		 *   2) convert to target endianness
+		 *   3) make changes
+		 *   4) convert back to host endianness
+		 *   5) write word back to target.
 		 */
-		uint32_t buffer;
+		retval = arc_jtag_read_memory(&arc32->jtag_info,
+		            (addr + i * sizeof(uint16_t)) & ~3u, &buffer_he);
+		target_buffer_set_u32(target, buffer_te, buffer_he);
+		/* buf is in host endianness, convert to target */
+		target_buffer_set_u16(target, halfword_te,
+                        *((uint16_t*)(buf + i * sizeof(uint16_t))));
+		memcpy(buffer_te  + ((addr + i * sizeof(uint16_t)) & 3u),
+                        halfword_te + i * sizeof(uint16_t), sizeof(uint16_t));
+		buffer_he = target_buffer_get_u32(target, buffer_te);
+		retval = arc_jtag_write_memory(&arc32->jtag_info,
+                        (addr + i * sizeof(uint16_t)) & ~3u, &buffer_he);
+	}
 
-		for(i = 0; i < count; i++) {
-			retval = arc_jtag_read_memory(jtag_info,
-					(addr + i * size) & ~3, &buffer);
-			memcpy(((void *) &buffer) + ((addr + i * size) & 3),
-				buf + i * size, size);
-			retval = arc_jtag_write_memory(jtag_info,
-				(addr + i * size) & ~3, &buffer);
-		}
+	return retval;
+}
+
+/* Write byte at address */
+static int arc_mem_write_block8(struct target *target, uint32_t addr, int count,
+	void *buf)
+{
+	struct arc32_common *arc32 = target_to_arc32(target);
+	int retval = ERROR_OK;
+	int i;
+
+	uint32_t buffer_he;
+	uint8_t buffer_te[sizeof(uint32_t)];
+	for(i = 0; i < count; i++) {
+		/* See comment in arc_mem_write_block16 for details. Since it is a byte
+		 * there is not need to convert write buffer to target endianness, but
+		 * we still have to convert read buffer. */
+		retval = arc_jtag_read_memory(&arc32->jtag_info, (addr + i) & ~3, &buffer_he);
+		target_buffer_set_u32(target, buffer_te, buffer_he);
+		memcpy(buffer_te  + ((addr + i) & 3), (uint8_t*)buf + i, 1);
+		buffer_he = target_buffer_get_u32(target, buffer_te);
+		retval = arc_jtag_write_memory(&arc32->jtag_info, (addr + i) & ~3, &buffer_he);
 	}
 
 	return retval;
@@ -79,20 +134,36 @@ int arc_mem_read(struct target *target, uint32_t address, uint32_t size,
 		return ERROR_TARGET_NOT_HALTED;
 	}
 
-	/* since we don't know if buffer is aligned, we allocate new mem that
-	 * is always aligned.
-	 */
-	void *tunnel;
+	/* Sanitize arguments */
+	if (((size != 4) && (size != 2) && (size != 1)) || (count == 0) || !(buffer))
+		return ERROR_COMMAND_SYNTAX_ERROR;
 
-	tunnel = malloc(count * size + 4);
-	if (!tunnel) {
+	if (((size == 4) && (address & 0x3u)) || ((size == 2) && (address & 0x1u)))
+	    return ERROR_TARGET_UNALIGNED_ACCESS;
+
+	void *tunnel_he;
+	uint8_t *tunnel_te;
+	uint32_t words_to_read, bytes_to_read;
+
+	/* TODO: I think this function might be made much more clear if it
+	 * would be splitted onto three based on size (4/2/1). That would
+	 * duplicate some logic, but it would be much easier to understand it,
+	 * those bit operations are just asking for a trouble. And they emulate
+	 * size-specific logic, that is smart, but dangerous.  */
+	/* Reads are word-aligned, so padding might be required if count > 1. */
+	bytes_to_read = (count * size + 3) & ~3u;
+	words_to_read = bytes_to_read >> 2;
+	tunnel_he = malloc(bytes_to_read);
+	tunnel_te = malloc(bytes_to_read);
+	if (!tunnel_he || !tunnel_te) {
 		LOG_ERROR("Out of memory");
 		return ERROR_FAIL;
 	}
 
-	retval = arc_mem_read_block(&arc32->jtag_info, address & ~3, 4,
-			((size * count) + 3) >> 2, tunnel);
-	
+	/* We can read only word-aligned words. */
+	retval = arc_mem_read_block(&arc32->jtag_info, address & ~3u, sizeof(uint32_t),
+		words_to_read, tunnel_he);
+
 	/* arc32_..._read_mem with size 4/2 returns uint32_t/uint16_t in host */
 	/* endianness, but byte array should represent target endianness      */
 
@@ -100,18 +171,26 @@ int arc_mem_read(struct target *target, uint32_t address, uint32_t size,
 		switch (size) {
 		case 4:
 			target_buffer_set_u32_array(target, buffer, count,
-						    tunnel + (address & 3));
+				tunnel_he);
 			break;
 		case 2:
-			target_buffer_set_u16_array(target, buffer, count,
-						    tunnel + (address & 3));
+			target_buffer_set_u32_array(target, tunnel_te,
+				words_to_read, tunnel_he);
+			/* Will that work properly with count > 1 and big endian? */
+			memcpy(buffer, tunnel_te + (address & 3u),
+				count * sizeof(uint16_t));
 			break;
 		case 1:
-			memcpy(buffer, tunnel + (address & 3), count);
+			target_buffer_set_u32_array(target, tunnel_te,
+				words_to_read, tunnel_he);
+			/* Will that work properly with count > 1 and big endian? */
+			memcpy(buffer, tunnel_te + (address & 3u), count);
+			break;
 		}
 	}
 
-	free(tunnel);
+	free(tunnel_he);
+	free(tunnel_te);
 
 	return retval;
 }
@@ -120,8 +199,6 @@ int arc_mem_write(struct target *target, uint32_t address, uint32_t size,
 	uint32_t count, const uint8_t *buffer)
 {
 	int retval = ERROR_OK;
-	struct arc32_common *arc32 = target_to_arc32(target);
-	struct arc_jtag *jtag_info = &arc32->jtag_info;
 
 	LOG_DEBUG("start writing @ address: 0x%8.8" PRIx32 " : %d times %d bytes",
 			address, count, size);
@@ -168,8 +245,16 @@ int arc_mem_write(struct target *target, uint32_t address, uint32_t size,
 		buffer = tunnel;
 	}
 
-	retval = arc_mem_write_block(jtag_info, address, size, count,
-			(void *)buffer);
+	if (size == 4) {
+		retval = arc_mem_write_block32(target, address, count, (void *)buffer);
+	} else if (size == 2) {
+		/* We convert buffer from host endianness to target. But then in
+		 * write_block16, we do the reverse. Is there a way to avoid this without
+		 * breaking other cases? */
+		retval = arc_mem_write_block16(target, address, count, (void *)buffer);
+	} else {
+		retval = arc_mem_write_block8(target, address, count, (void *)buffer);
+	}
 
 	if (tunnel != NULL)
 		free(tunnel);
