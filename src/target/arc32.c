c73d6b1587970471bebf6308853d098bd64aef0c
Anton Kolesov <Anton.Kolesov@synopsys.com>
2014-11-27 16:13:55 +0300

    arc: Remove arc.{c,h} files
    There were two small to justify existence, especially since we have only
    one target "arc32". Content integrated into arc32.{c,h} files.
    
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- src/target/arc32.c
+++ src/target/arc32.c
@@ -24,7 +24,7 @@
 #include "config.h"
 #endif
 
-#include "arc.h"
+#include "arc32.h"
 
 /* ----- Supporting functions ---------------------------------------------- */
 
@@ -594,4 +594,56 @@ int arc32_configure(struct target *target)
 
 	return ERROR_OK;
 }
+
+/* ARC target */
+struct target_type arc32_target = {
+	.name = "arc32",
+
+	.poll =	arc_ocd_poll,
+
+	.arch_state = arc32_arch_state,
+
+	/* TODO That seems like something similiar to metaware hostlink, so perhaps
+	 * we can exploit this in the future. */
+	.target_request_data = NULL,
+
+	.halt = arc_dbg_halt,
+	.resume = arc_dbg_resume,
+	.step = arc_dbg_step,
+
+	.assert_reset = arc_ocd_assert_reset,
+	.deassert_reset = arc_ocd_deassert_reset,
+
+	/* TODO Implement soft_reset_halt */
+	.soft_reset_halt = NULL,
+
+	.get_gdb_reg_list = arc_regs_get_gdb_reg_list,
+
+	.read_memory = arc_mem_read,
+	.write_memory = arc_mem_write,
+	.checksum_memory = arc_mem_checksum,
+	.blank_check_memory = arc_mem_blank_check,
+
+	.add_breakpoint = arc_dbg_add_breakpoint,
+	.add_context_breakpoint = arc_dbg_add_context_breakpoint,
+	.add_hybrid_breakpoint = arc_dbg_add_hybrid_breakpoint,
+	.remove_breakpoint = arc_dbg_remove_breakpoint,
+	.add_watchpoint = arc_dbg_add_watchpoint,
+	.remove_watchpoint = arc_dbg_remove_watchpoint,
+
+	.run_algorithm = arc_mem_run_algorithm,
+	.start_algorithm = arc_mem_start_algorithm,
+	.wait_algorithm = arc_mem_wait_algorithm,
+
+	.commands = arc_monitor_command_handlers, /* see: arc_mntr.c|.h */
+
+	.target_create = arc_ocd_target_create,
+	.init_target = arc_ocd_init_target,
+	.examine = arc_ocd_examine,
+
+	.virt2phys = arc_mem_virt2phys,
+	.read_phys_memory = arc_mem_read_phys_memory,
+	.write_phys_memory = arc_mem_write_phys_memory,
+	.mmu = arc_mem_mmu,
+};
 
