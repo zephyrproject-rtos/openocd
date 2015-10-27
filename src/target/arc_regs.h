5c527d1544109c02e8212322c1a2c301d1f499e3
Anton Kolesov <Anton.Kolesov@synopsys.com>
2015-10-27 21:31:37 +0300

    arc: Add support for address actionpoints
    - Instruction breaks: just remove hardcoded values
    - Memory breaks: implemented like watchpoints
    - AuxReg breaks: two new commands to set and remove them, currently no
      masking implemented for any of them.
    
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- src/target/arc_regs.h
+++ src/target/arc_regs.h
@@ -65,6 +65,21 @@
 #define SET_CORE_DISABLE_INTERRUPTS		0x00000000
 #define SET_CORE_ENABLE_INTERRUPTS		0xFFFFFFFF
 
+ /* Action Point */
+#define AP_AMV_BASE				0x220
+#define AP_AMM_BASE				0x221
+#define AP_AC_BASE				0x222
+#define AP_STRUCT_LEN			0x3
+
+#define AP_AC_AT_INST_ADDR		0x0
+#define AP_AC_AT_MEMORY_ADDR	0x2
+#define AP_AC_AT_AUXREG_ADDR	0x4
+
+#define AP_AC_TT_DISABLE		0x00
+#define AP_AC_TT_WRITE			0x10
+#define AP_AC_TT_READ			0x20
+#define AP_AC_TT_READWRITE		0x30
+
 struct arc32_reg_desc {
 	uint32_t regnum;
 	char * const name;
