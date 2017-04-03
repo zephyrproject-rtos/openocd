cb78e1304bfd144159e4bb18a5b9127b20ccbb92
Anton Kolesov <Anton.Kolesov@synopsys.com>
2017-04-03 14:53:06 +0300

    arc: Flush L2 cache at reset for ARC HS
    ARC port of OpenOCD currently doesn't support L2 cache - it is not flushed
    and invalidated when memory is being modified.  However that is not needed
    for a simplified case where OpenOCD is needed only to do an initial load of
    image into target and then let it run. Still, to be sure that this operation
    is safe it is preferably to flush and invalidate L2 cache before loading
    image. This can be easily done in the TCL configuration script, for example
    in in reset handler.
    
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- tcl/cpu/arc/hs.tcl
+++ tcl/cpu/arc/hs.tcl
@@ -33,4 +33,37 @@ proc arc_hs_init_regs { } {
 # invocation of arc_common_reset.
 proc arc_hs_reset { {target ""} } {
 	arc_v2_reset $target
+
+	# Invalidate L2 cache if there is one.
+	set l2_config [$target arc jtag aux-reg 0x901]
+	# Will return 0, if cache is not present and register doesn't exist.
+	set l2_ctrl [$target arc jtag aux-reg 0x903]
+	if { ($l2_config != 0) && (($l2_ctrl & 1) == 0) } {
+		puts "L2 cache is present and not disabled"
+
+		# Wait until BUSY bit is 0.
+		puts "Invalidating L2 cache..."
+		$target arc jtag aux-reg 0x905 1
+		# Dummy read of SLC_AUX_CACHE_CTRL bit, as described in:
+		# https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/commit/arch/arc?id=c70c473396cbdec1168a6eff60e13029c0916854
+		set l2_ctrl [$target arc jtag aux-reg 0x903]
+		set l2_ctrl [$target arc jtag aux-reg 0x903]
+		while { ($l2_ctrl & 0x100) != 0 } {
+			set l2_ctrl [$target arc jtag aux-reg 0x903]
+		}
+
+		# Flush cache if needed. If SLC_AUX_CACHE_CTRL.IM is 1, then invalidate
+		# operation already flushed everything.
+		if { ($l2_ctrl & 0x40) == 0 } {
+			puts "Flushing L2 cache..."
+			$target arc jtag aux-reg 0x904 1
+			set l2_ctrl [$target arc jtag aux-reg 0x903]
+			set l2_ctrl [$target arc jtag aux-reg 0x903]
+			while { [expr $l2_ctrl & 0x100] != 0 } {
+				set l2_ctrl [$target arc jtag aux-reg 0x903]
+			}
+		}
+
+		puts "L2 cache has been flushed and invalidated."
+	}
 }
