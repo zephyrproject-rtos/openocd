7775fc9756a9233218b408b437638d299f425fe1
Jason Yu <xuzj03@gmail.com>
2018-10-30 06:33:36 +0800

    [RV32M1] Add RV32M1 spport
     - Add nor flash files
     - Add target files
    
    Signed-off-by: Jason Yu <xuzj03@gmail.com>
    
    [ Pulled in from https://github.com/open-isa-rv32m1/rv32m1-openocd ]
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- src/target/target.c
+++ src/target/target.c
@@ -116,6 +116,7 @@ extern struct target_type stm8_target;
 extern struct target_type riscv_target;
 extern struct target_type mem_ap_target;
 extern struct target_type esirisc_target;
+extern struct target_type rv32m1_target;
 
 static struct target_type *target_types[] = {
 	&arm7tdmi_target,
@@ -160,6 +161,7 @@ static struct target_type *target_types[] = {
 #if BUILD_TARGET64
 	&aarch64_target,
 #endif
+	&rv32m1_target,
 	NULL,
 };
 
