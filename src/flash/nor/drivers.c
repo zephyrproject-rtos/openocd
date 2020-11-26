1e480b719e0762353d975bc061798c3546bbb6b8
Wealian Liao <WHLIAO@nuvoton.com>
2020-11-26 10:25:09 +0800

    flash/nor: add support for Nuvoton NPCX series flash
    Added NPCX flash driver to support the Nuvoton NPCX series
    microcontrollers. Add config file for NPCX series.
    
    Change-Id: Ia10b019a3521f59ad1e10ccdc56827ba30c3eac8
    Signed-off-by: Wealian Liao <WHLIAO@nuvoton.com>
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>
    Reviewed-on: https://review.openocd.org/c/openocd/+/5950
    Tested-by: jenkins
    Reviewed-by: Oleksij Rempel <linux@rempel-privat.de>

--- src/flash/nor/drivers.c
+++ src/flash/nor/drivers.c
@@ -57,6 +57,7 @@ extern const struct flash_driver mdr_flash;
 extern const struct flash_driver mrvlqspi_flash;
 extern const struct flash_driver msp432_flash;
 extern const struct flash_driver niietcm4_flash;
+extern const struct flash_driver npcx_flash;
 extern const struct flash_driver nrf5_flash;
 extern const struct flash_driver nrf51_flash;
 extern const struct flash_driver numicro_flash;
@@ -132,6 +133,7 @@ static const struct flash_driver * const flash_drivers[] = {
 	&mrvlqspi_flash,
 	&msp432_flash,
 	&niietcm4_flash,
+	&npcx_flash,
 	&nrf5_flash,
 	&nrf51_flash,
 	&numicro_flash,
